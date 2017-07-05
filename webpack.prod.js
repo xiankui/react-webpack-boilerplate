666e22e50ea91c247404f2d7fce2efdcfc5fae87
Kevin Zhu(朱献奎) <CI12293@careerintlinc.local>
2017-07-05 15:48:16 +0800

    fix production env issue

--- webpack.prod.js
+++ webpack.prod.js
@@ -81,6 +81,13 @@ module.exports = {
     ]
 	},
 	plugins: [
+		// [fix issue](https://github.com/facebook/react/issues/6479)
+		new webpack.DefinePlugin({
+		  "process.env": { 
+		     NODE_ENV: JSON.stringify("production") 
+		   }
+		}),
+
 		new webpack.LoaderOptionsPlugin({
 		    minimize: true,
 		    debug: false
